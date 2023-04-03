ae12fe1dfde9d20d275d6d086a53391552e4239b
Jean <64726220+jeanth20@users.noreply.github.com>
2023-04-03 16:21:57 +0200

    Add files via upload

--- servers.py
+++ servers.py
@@ -18,72 +18,6 @@ current_directory = os.getcwd()
 current_path = os.path.abspath(os.getcwd())
 FolderPath = os.getcwd()
 
-
-@app.get("/cwd")
-async def cwd():
-    allpaths = { 
-    "current_directory": current_directory,
-    "current_path": current_path,
-    }
-    return(allpaths)
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-
-@app.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    with open(file.filename, 'wb') as image:
-        content = await file.read()
-        image.write(content)
-        image.close()
-    return JSONResponse(content={"filename": file.filename},
-status_code=200)
-
-
-@app.get("/download/{name_file}")
-def download_file(name_file: str):
-    return FileResponse(path=getcwd() + "/" + name_file, media_type='application/octet-stream', filename=name_file)
-
-
-# @app.post("/directory")
-# async def download_post(request: Request,):
-#     absPath = Path(current_path)
-#     def fObjFromScan(x):
-#         fileStat = x.stat()
-#         # return file information for rendering
-#         return {'name': x.name,
-#                 'fIcon': "bi bi-folder-fill" if x.is_dir() else getIconClassForFilename(x.name),
-#                 'relPath': str(x.relative_to(Path(FolderPath))).replace("\\", "/"),
-#                 'mTime': getTimeStampString(fileStat.st_mtime),
-#                 'size': getReadableByteSize(fileStat.st_size)}
-#     fileList = [fObjFromScan(f) for f in absPath.iterdir()]
-#     return(fileList)
-    # return templates.TemplateResponse("directory.html", {"request": request, "dirPath": str(absPath), "fileList": fileList})
-
-
-@app.get("/file/{name_file}")
-def get_file(name_file: str):
-    return FileResponse(path=getcwd() + "/" + name_file)
-
-
-@app.delete("/delete/file/{name_file}")
-def delete_file(name_file: str):
-    try:
-        remove(getcwd() + "/" + name_file)
-        return JSONResponse(content={
-            "removed": True
-            }, status_code=200)   
-    except FileNotFoundError:
-        return JSONResponse(content={
-            "removed": False,
-            "error_message": "File not found"
-        }, status_code=404)
-
-
-FolderPath = os.getcwd()
-
 def getReadableByteSize(num, suffix="B") -> str:
     for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
         if abs(num) < 1024.0:
@@ -111,41 +45,108 @@ def getIconClassForFilename(fName):
     return fileIconClass
 
 
-@app.get("/directory")
-async def download_post(request: Request, path: Union[str, None] = FolderPath):
-    absPath = Path(FolderPath) / path
-    def fObjFromScan(x):
-        fileStat = x.stat()
+@app.get("/cwd")
+async def cwd():
+    allpaths = { 
+    "current_directory": current_directory,
+    "current_path": current_path,
+    }
+    return(allpaths)
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    with open(file.filename, 'wb') as image:
+        content = await file.read()
+        image.write(content)
+        image.close()
+    return JSONResponse(content={"filename": file.filename},
+status_code=200)
+
+
+@app.get("/download/{name_file}")
+def download_file(name_file: str):
+    return FileResponse(path=getcwd() + "/" + name_file, media_type='application/octet-stream', filename=name_file)
+
+
+@app.get("/file/{name_file}")
+def get_file(name_file: str):
+    return FileResponse(path=getcwd() + "/" + name_file)
+
+
+@app.delete("/delete/file/{name_file}")
+def delete_file(name_file: str):
+    try:
+        remove(getcwd() + "/" + name_file)
+        return JSONResponse(content={
+            "removed": True
+            }, status_code=200)   
+    except FileNotFoundError:
+        return JSONResponse(content={
+            "removed": False,
+            "error_message": "File not found"
+        }, status_code=404)
+
+
+
+
+
+
+
+
+
+
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from pathlib import Path
+from typing import Union
+
+
+@app.get("/reports/{path:path}")
+@app.get("/reports/")
+async def get_files(request: Request, path: Union[str, None] = None):
+    abs_path = Path(FolderPath) / path if path else Path(FolderPath)
+    file_list = []
+    for f in abs_path.iterdir():
+        file_stat = f.stat()
+        file_obj = {
+            "name": f.name,
+            "fIcon": "bi bi-folder-fill" if f.is_dir() else getIconClassForFilename(f.name),
+            "relPath": str(f.relative_to(Path(FolderPath))).replace("\\", "/"),
+            "mTime": getTimeStampString(file_stat.st_mtime),
+            "size": getReadableByteSize(file_stat.st_size),
+        }
+        if f.is_dir():
+            file_obj["hasFiles"] = any(f.is_file() for f in f.iterdir())
+        file_list.append(file_obj)
+    return JSONResponse(content=file_list)
+
+
+@app.post('/reports/')
+async def get_files(request: Request):
+    data = await request.json()
+    req_path = data['path']
+
+    abs_path = Path(FolderPath) / req_path
+
+    def f_obj_from_scan(x):
+        file_stat = x.stat()
         # return file information for rendering
         return {
             "name": x.name,
             "fIcon": "bi bi-folder-fill" if x.is_dir() else getIconClassForFilename(x.name),
             "relPath": str(x.relative_to(Path(FolderPath))).replace("\\", "/"),
-            "mTime": getTimeStampString(fileStat.st_mtime),
-            "size": getReadableByteSize(fileStat.st_size),
+            "mTime": getTimeStampString(file_stat.st_mtime),
+            "size": getReadableByteSize(file_stat.st_size),
         }
-    fileList = [fObjFromScan(f) for f in absPath.iterdir()]
-    return JSONResponse(content=fileList)
+
+    file_list = [f_obj_from_scan(f) for f in abs_path.iterdir()]
+
+    return JSONResponse(content=file_list)
 
 
-# @app.post("/directory")
-# async def download_post(request: Request, path: Union[str, None] = FolderPath):
-#     absPath = Path(FolderPath) / path
-#     def fObjFromScan(x):
-#         fileStat = x.stat()
-#         # return file information for rendering
-#         return {
-#             "name": x.name,
-#             "fIcon": "bi bi-folder-fill" if x.is_dir() else getIconClassForFilename(x.name),
-#             "relPath": str(x.relative_to(Path(FolderPath))).replace("\\", "/"),
-#             "mTime": getTimeStampString(fileStat.st_mtime),
-#             "size": getReadableByteSize(fileStat.st_size),
-#         }
-#     fileList = [fObjFromScan(f) for f in absPath.iterdir()]
-#     return JSONResponse(content=fileList)
-
-
-@app.get('/files-display')
+@app.post('/files-display/{item}')
 def displayfiles():
     html_content = """
     <html>
@@ -163,8 +164,11 @@ def displayfiles():
             th {text-align: left;}
         </style>
         <script>
+            subdirectory_path = "";
+        
           $.ajax({
-          url: '/directory/',
+          url: '/reports/' + subdirectory_path,
+          //url: '/reports/',
           type: 'GET',
           success: function(response) {
               $('#directory').append(
@@ -176,14 +180,14 @@ def displayfiles():
                   num = i + 1;
                   $('#directory').append(
                       "<tr>"+
-                      //"    <td scope='row'>"+num+"</td>"+
+                      "    <td scope='row'>"+num+"</td>"+
                       "    <td>"+
-                      "        <a href='' target='blank'>"+
-                      "            <span><i class='"+item.icon+"' style='margin-right:0.3em'></i></span>"+
+                      "        <a href='http://127.0.0.1:8000/files-display/"+item.relPath+"' >"+
+                      "            <span><i class='"+item.fIcon+"' style='margin-right:0.3em'></i></span>"+
                       "        </a>"+
                       "    </td>"+
                       "    <th> "+item.name+" </th>"+
-                      "    <th> "+item.time+" </th>"+
+                      "    <th> "+item.mTime+" </th>"+
                       "    <th> "+item.size+" </th>"+
                       "</tr>"
                   );
@@ -208,3 +212,73 @@ def displayfiles():
     """
     
     return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.get('/files-display')
+def displayfiles():
+    html_content = """
+    <html>
+        <body>
+            <div id='directory' class='table table-striped table-responsive'></div>
+        </body>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css'>
+        <style>
+            table {table-layout:fixed; width:100%;}
+            table td, th {word-wrap:break-word;}
+            th {text-align: left;}
+        </style>
+        <script>
+            subdirectory_path = "";
+        
+          $.ajax({
+          url: '/reports/' + subdirectory_path,
+          //url: '/reports/',
+          type: 'GET',
+          success: function(response) {
+              $('#directory').append(
+                  "<table>"+
+                  "<tbody>"
+              );
+
+              $.each(response, function(i, item) {
+                  num = i + 1;
+                  $('#directory').append(
+                      "<tr>"+
+                      "    <td scope='row'>"+num+"</td>"+
+                      "    <td>"+
+                      "        <a href='http://127.0.0.1:8000/files-display/"+item.relPath+"' >"+
+                      "            <span><i class='"+item.fIcon+"' style='margin-right:0.3em'></i></span>"+
+                      "        </a>"+
+                      "    </td>"+
+                      "    <th> "+item.name+" </th>"+
+                      "    <th> "+item.mTime+" </th>"+
+                      "    <th> "+item.size+" </th>"+
+                      "</tr>"
+                  );
+              });
+
+              $('#directory').append(
+                  "    </tbody>"+
+                  "</table>"+
+                  "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css'>"+
+                  "<style>"+
+                  "    table {table-layout:fixed; width:100%;}"+
+                  "    table td, th {word-wrap:break-word;}"+
+                  "    th {text-align: left;}"+
+                  "</style>");
+            }
+          });
+      </script>
+
+      <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css'>
+
+    </html>
+    """
+    
+    return HTMLResponse(content=html_content, status_code=200)
+
+
